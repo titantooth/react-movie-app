77b84221c289dc6c3d17a447f97cfc7660567c25
titantooth <ali.rabieeie@gmail.com>
2023-08-01 23:33:30 +0330

    edu site for sale

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ListGroup from "./components/ListGroup";
 function App() {
-  return <div><ListGroup/></div>;
+  const Items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  return <div><ListGroup Items={Items} heading="Cities" /></div>;
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,35 @@
-function ListGroup() {
-  const Items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// import { MouseEvent } from "react";
+interface ListGroupProps {
+  Items: string[];
+  heading: string;
+}
 
-  if (Items.length === 0) return <p>No items found.</p>;
 
+import { useState } from "react";
+
+
+function ListGroup({ Items, heading }: ListGroupProps) {
+
+  //hook
+  const [SelectedIndex, setSelectedIndex] = useState(-1);
+
+
+  const message = Items.length === 0 ? <p>No item found!</p> : null;
+
+  //event handler//
+  // const HandleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {message}
+      {/* {Items.length === 0 && <p>no item found</p> } */}
       <ul className="list-group">
-        {Items.map((item) => (
-          <li id={item} className="list-group-item">
+        {Items.map((item, index) => (
+          <li
+            id={item}
+            onClick={() => { setSelectedIndex(index) }}
+            className={SelectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+          >
             {item}
           </li>
         ))}
